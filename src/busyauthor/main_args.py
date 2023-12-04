1e64bdb579bf867fbd5da12968aa451589319381
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-04 09:59:25 -0800

    failing

--- src/busyauthor/main_args.py
+++ src/busyauthor/main_args.py
@@ -2,6 +2,7 @@ import argparse
 
 from busyauthor import __version__
 from busyauthor import args_common as argsmod
+from busyauthor import command
 
 parser = argparse.ArgumentParser(
     description="Just a command, sub command, subsub command demonstration"
@@ -9,24 +10,25 @@ parser = argparse.ArgumentParser(
 argsmod.add_common_args(parser)
 
 
-parser.add_argument(
-    "--version",
-    action="version",
-    version=f"busyauthor {__version__}",
-)
-parser.add_argument("--db", help="Specify the database file (e.g., data.cypher)")
+def add_arguments(parser):
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"busyauthor {__version__}",
+    )
+    parser.add_argument("--db", help="Specify the database file (e.g., data.cypher)")
 
 
-subparsers = parser.add_subparsers(
-    description="valid commands",
-    title="command",
-    help="command help",
-    required=True,
-    dest="command",
-)
+def add_parser(subparsers):
+    parser = subparsers.add_parser(
+        "down",
+        help=(
+            "download url and expand (if zip) into cwd.  Example: {cmd} down "
+            "https://www.dropbox.com/s/zirk55k8ty0dy1i/Spectra.prm.2.0.4.zip?dl=0"
+        ),
+    )
+    add_arguments(parser)
 
-command_parser = subparsers.add_parser(
-    "command", aliases=["cmd"], help="Top-level command"
-)
 
-command_parser.add_argument("--command-args", help="Arguments for the command")
+add_arguments(parser)
+add_parser(command.subparsers)
