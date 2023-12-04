cc6fe503b39e864e0f98ee9683122a36e6bb99cb
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-04 12:23:41 -0800

    seems ok

--- src/busyauthor/main_args.py
+++ src/busyauthor/main_args.py
@@ -1,12 +1,11 @@
 import argparse
 
-from busyauthor import __version__
-from busyauthor import args_common as argsmod
+from busyauthor import __version__, args_common
 
 parser = argparse.ArgumentParser(
     description="Just a command, sub command, subsub command demonstration"
 )
-argsmod.add_common_args(parser)
+args_common.add_common_args(parser)
 
 
 parser.add_argument(
@@ -15,27 +14,3 @@ parser.add_argument(
     version=f"busyauthor {__version__}",
 )
 parser.add_argument("--db", help="Specify the database file (e.g., data.cypher)")
-
-
-subparsers = parser.add_subparsers(
-    description="valid commands",
-    title="command",
-    help="command help",
-    required=True,
-    dest="command",
-)
-
-command_parser = subparsers.add_parser(
-    "command", aliases=["cmd"], help="Top-level command"
-)
-command_parser.add_argument("--command-args", help="Arguments for the command")
-argsmod.add_common_args(command_parser)
-
-subcommand_parser = command_parser.add_subparsers(
-    dest="subcommand", help="Available subcommands"
-)
-subcommand_parser = subcommand_parser.add_parser(
-    "subcommand", aliases=["subcmd"], help="Subcommand"
-)
-subcommand_parser.add_argument("--subcommand-args", help="Arguments for the subcommand")
-argsmod.add_common_args(subcommand_parser)
