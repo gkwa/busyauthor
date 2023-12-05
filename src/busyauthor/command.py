92bf8e509e82ec155776150a1bd374ea667c06b5
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-04 20:56:33 -0800

    Revert "Add cmd2"
    This reverts commit 66cd7793f80d06e8b973232436ca81e815463f21.

--- src/busyauthor/command.py
+++ src/busyauthor/command.py
@@ -18,11 +18,4 @@ def add_subparsers(parser):
 
     subcommand.add_subparsers(parser)
 
-    parser = subparsers.add_parser(
-        "command2",
-        help="command2 help",
-        aliases=["cmd2"],
-    )
-    add_arguments(parser)
-
     return parser
